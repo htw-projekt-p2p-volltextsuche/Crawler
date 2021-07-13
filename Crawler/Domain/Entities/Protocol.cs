b64b1685fde18450bebbd36aebb32b88f550dc92
Ryan Torzynski <ryantorzynski@gmail.com>
2021-07-13 20:01:02 +0200

    Corrected names of mongodb elements

--- Crawler/Domain/Entities/Protocol.cs
+++ Crawler/Domain/Entities/Protocol.cs
@@ -10,18 +10,23 @@ namespace Crawler.Domain.Entities
         [JsonPropertyName("doc_id")]
         public string Id { get; set; }
 
+        [BsonElement("title")]
         [JsonPropertyName("title")]
         public string Title { get; set; }
 
+        [BsonElement("speaker")]
         [JsonPropertyName("speaker")]
         public string Speaker { get; set; }
 
+        [BsonElement("affiliation")]
         [JsonPropertyName("affiliation")]
         public string Affiliation { get; set; }
 
+        [BsonElement("date")]
         [JsonPropertyName("date")]
         public string Date { get; set; }
 
+        [BsonElement("text")]
         [JsonPropertyName("text")]
         public string Text { get; set; }
     }
